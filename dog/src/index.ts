fd0f48ce747151804e2108935bf74315409a7a90
NickLewry <nicholas.lewry@icloud.com>
2019-04-24 12:53:42 +0100

    setup docker

--- dog/src/index.ts
+++ dog/src/index.ts
@@ -6,7 +6,7 @@ import { RedisCache } from "./cache";
 import app from "./app";
 
 const main = async () => {
-  const cache = new RedisCache(redis.createClient(6379));
+  const cache = new RedisCache(redis.createClient({ host: 'redis' }));
   console.log("starting app");
   http.createServer(app(requestPromiseNative, cache).callback()).listen(8080);
   console.log("listening port 8080");
